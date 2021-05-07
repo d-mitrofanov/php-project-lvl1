8267f4407d8e9c7a172941301b01a9a98c2b050e
d-mitrofanov <mitrowork@gmail.com>
2021-05-07 19:56:47 +0300

    undefined func fix gcd

--- src/Games/BrainGCD.php
+++ src/Games/BrainGCD.php
@@ -4,6 +4,16 @@ namespace Brain\Games\Games\BrainGCD;
 
 use function Brain\Games\Engine\startGame;
 
+function getGCD($a, $b)
+{
+    while ($b !== 0) {
+        $m = $a % $b;
+        $a = $b;
+        $b = $m;
+    }
+    return $a;
+}
+
 function run()
 {
     $description = "Find the greatest common divisor of given numbers.";
@@ -11,7 +21,7 @@ function run()
         $num1 = rand(1, 100);
         $num2 = rand(1, 100);
         $question = "{$num1} {$num2}";
-        $answer = gmp_gcd($num1, $num2);
+        $answer = getGCD($num1, $num2);
         return [$question, strval($answer)];
     };
 
